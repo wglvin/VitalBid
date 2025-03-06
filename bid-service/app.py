9f54325167edfe78771cb0a4a5ef7941eed8b302
wglvin <vincentwang.2023@scis.smu.edu.sg>
2025-03-06 16:34:20 +0800

    password changes

--- bid-service/app.py
+++ bid-service/app.py
@@ -4,6 +4,6 @@ import mysql.connector
 db = mysql.connector.connect(
     host=os.getenv("DB_HOST", "localhost"),
     user=os.getenv("DB_USER", "root"),
-    password=os.getenv("DB_PASSWORD", "password"),
+    password=os.getenv("DB_PASSWORD", ""),
     database=os.getenv("DB_NAME", "organ_marketplace")
 )
