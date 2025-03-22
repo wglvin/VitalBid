a0135b3cc2d3140851fc10e2373a9912b0017dce
moseskng <moseskecm@gmail.com>
2025-03-22 16:29:51 +0800

    notification: added gitignore file

--- notification/config/config.js
+++ notification/config/config.js
@@ -12,9 +12,9 @@ module.exports = {
         }
     },
     mailgun: {
-        apiKey: process.env.MAILGUN_API_KEY || '9365d12bb9c9e3da766d697175b6c81a-3d4b3a2a-c2781184',
-        domain: process.env.MAILGUN_DOMAIN || 'sandboxf1dca3939f8f433d966ab0aacf7d7dac.mailgun.org',
-        from: process.env.EMAIL_FROM || 'postmaster@sandboxf1dca3939f8f433d966ab0aacf7d7dac.mailgun.org'
+        apiKey: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN,
+        from: process.env.EMAIL_FROM || 'noreply@example.com'
     },
     notificationProviders: {
         direct: process.env.DIRECT_NOTIFICATION_URL || 'http://push-notification-service:3005/send',
